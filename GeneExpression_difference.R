a82197a6148be97d3b41b92a9b846c651c933eb8
wigge206 <wiggs38@gmail.com>
2020-04-23 15:51:27 +1200

    Update GeneExpression_difference.R

--- GeneExpression_difference.R
+++ GeneExpression_difference.R
@@ -1,7 +1,38 @@
 # Code calls a function which runs var.test, levene test (lawstat) and DE (limma) based on a binary definer
 # e.g. A vector is made where by a 1 symbols the first group and 0 the second. order matches samples in df
-source("~/Projects/PhD/Bioinformatics/Scripts/Functions/Expression_variabilityTest.R")
-
+library(lawstat)
+library(limma)
+## Function to preform dvar and DE, pvalue correction is FDR by default
+ExpVar.test <- function(data, classifier, p.adj = "BH"){
+  Ftest = apply(data,1, function(x) var.test(x[classifier == 1],x[classifier == 0]))
+  LevTest = apply(data, 1, function(x) levene.test(x, classifier, location="median", correction.method="zero.correction")) 
+  
+  df = data.frame(row.names = rownames(data), F = unlist(lapply(Ftest, function(x) x$statistic)),
+                  p.value = unlist(lapply(Ftest, function(x) x$p.value)),
+                  lev.t.statistic = unlist(lapply(LevTest, function(x) x$statistic)),
+                  lev.p.value = unlist(lapply(LevTest, function(x) x$p.value)))
+  df$lev.adj.p.value = p.adjust(df$lev.p.value, method = p.adj, n = nrow(df))
+  
+  
+  classifier = ifelse(classifier == 1, "G1","G2")
+  fgroup = factor(classifier)
+  
+  design = model.matrix(~ fgroup + 0, data)
+  colnames(design) = levels(fgroup)
+  rownames(design) = colnames(data)
+  
+  fit = lmFit(data, design)
+  cont.matrix = makeContrasts(G1-G2, levels=design)
+  fit2 = contrasts.fit(fit, cont.matrix)
+  fit2 = eBayes(fit2, 0.01)
+  x = topTable(fit2, adjust=p.adj, sort.by="B", number=nrow(df))
+  x = x[match(row.names(df), row.names(x)),]
+  colnames(x)[3:6] = paste("limma", colnames(x)[3:6], sep =".")
+  
+  df = cbind(df, x)
+  
+  return(df)
+}
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
 ### Waddell expression stats (DVAR AND DE)
